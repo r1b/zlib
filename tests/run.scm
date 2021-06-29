8405d46ed9ed408f346509983e4d72d3e127a488
Vasilij Schneidermann <mail@vasilij.de>
2021-06-29 08:30:52 +0200

    Add deflate/zlib/gzip tests

--- tests/run.scm
+++ tests/run.scm
@@ -1,5 +1,6 @@
 
-(import (chicken file)
+(import (chicken bitwise)
+        (chicken file)
         (chicken file posix)
         (chicken io)
         (chicken port)
@@ -16,7 +17,37 @@
 (define in-data (string-unfold null? car cdr (list-tabulate in-data-size (lambda (i) (integer->char (pseudo-random-integer 255))))))
 (define sha1 (string->sha1sum in-data))
 
+;; taken from python's zlib wrapper
+(define expected "test")
+(define expected-deflate "+I-.\x01\x00")
+(define expected-zlib "x\xda+I-.\x01\x00\x04")
+(define expected-gzip "\x1f\x8b\x08\x00\x00\x00\x00\x00\x02\x03+I-.\x01\x00\x0c~\x7f\xd8\x04\x00\x00\x00")
+
 (define out-data "")
+
+(define (compress str #!optional (wbits zlib/max-wbits))
+  (call-with-output-string
+   (lambda (out)
+     (let ((out (open-zlib-compressed-output-port out window-bits: wbits)))
+       (write-string str #f out)
+       (close-output-port out)))))
+
+(define (compress-deflate str) (compress str (- zlib/max-wbits)))
+(define (compress-zlib str) (compress str zlib/max-wbits))
+(define (compress-gzip str) (compress str (bitwise-ior zlib/max-wbits 16)))
+
+(define (decompress str #!optional (wbits zlib/max-wbits))
+  (call-with-output-string
+   (lambda (out)
+     (call-with-input-string str
+       (lambda (in)
+         (let ((in (open-zlib-compressed-input-port in window-bits: wbits)))
+           (write-string (read-string #f in) #f out)))))))
+
+(define (decompress-deflate str) (decompress str (- zlib/max-wbits)))
+(define (decompress-zlib str) (decompress str zlib/max-wbits))
+(define (decompress-gzip str) (decompress str (bitwise-ior zlib/max-wbits 16)))
+(define (decompress-auto str) (decompress str (bitwise-ior zlib/max-wbits 32)))
 
 ;; test for idempotence
 
@@ -24,23 +55,21 @@
 
 (test-assert "deflate"
              (begin
-               (set! out-data
-                     (let ((string-port (open-output-string)))
-                       (with-output-to-port (open-zlib-compressed-output-port string-port)
-                         (lambda ()
-                           (write-string in-data)
-                           (close-output-port (current-output-port))
-                           (get-output-string string-port)))))
+               (set! out-data (compress-zlib in-data))
                (> (string-length out-data) #x10000)))
+(test "inflate" sha1 (string->sha1sum (decompress out-data)))
 
-(test "inflate" sha1
-      (string->sha1sum
-       (let ((string-port (open-output-string)))
-         (with-input-from-port (open-zlib-compressed-input-port (open-input-string out-data))
-           (lambda ()
-             (write-string (read-string) #f string-port)
-             (close-input-port (current-input-port))
-             (get-output-string string-port))))))
+(test "deflate (default)" expected (decompress (compress expected)))
+(test "deflate (deflate)" expected (decompress-deflate (compress-deflate expected)))
+(test "deflate (zlib)" expected (decompress-zlib (compress-zlib expected)))
+(test "deflate (gzip)" expected (decompress-gzip (compress-gzip expected)))
+
+(test "inflate (default)" expected (decompress expected-zlib))
+(test "inflate (deflate)" expected (decompress-deflate expected-deflate))
+(test "inflate (zlib)" expected (decompress-zlib expected-zlib))
+(test "inflate (gzip)" expected (decompress-gzip expected-gzip))
+(test "inflate (auto, zlib)" expected (decompress-auto expected-zlib))
+(test "inflate (auto, gzip)" expected (decompress-auto expected-gzip))
 
 (test-end)
 
